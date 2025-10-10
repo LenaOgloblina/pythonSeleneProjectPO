3cb598ed58569350d37163da537d7923da6c3dd5
Elena Oglobina <eogloblina@outlook.com>
2025-10-10 11:49:33 +0500

    Описала заполненеие 3х полей

--- tests/test_user_form.py
+++ tests/test_user_form.py
@@ -1,4 +1,8 @@
 from selene import browser
 
 def test_user_page():
-    browser.open("")
+    browser.open("https://demoqa.com/automation-practice-form")
+    browser.element('#firstName').type('Ирина')
+    browser.element('#lastName').type('Иванова')
+    browser.element('#lastName').type('test132@gmail.com')
+    browser.all('#genterWrapper > .col-md-9').element('')
